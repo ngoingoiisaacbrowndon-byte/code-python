a5a449e4c95082d926aacf30a2225b5de8cb8933
isaac browndon <ngoingoiisaacbrowndn@gmail.com>
2026-06-08 12:30:52 +0200

    isaac

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-liste = []
-print("remplissage du tableau")
-for i in range(1 ,10) :
-    N = int(input("input(entrer N n°{i} : "))
-    liste.append
-    print(f"le plus petit : {le plus petit}")
-    print(f"le plus petit : {le plus grand}")
-    #commentaire de test
+N = int(input("entrer N :"))
+somme = 0
+for i in range(0 , N-1)
+print("entrer N° ")i+1
+somme = somme + liste{}
