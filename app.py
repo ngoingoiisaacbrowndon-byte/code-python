d72b999b078c4dd4b1d38c0a66048b53fa917de7
isaac browndon <ngoingoiisaacbrowndn@gmail.com>
2026-06-08 16:31:26 +0200

    ajout tableau min et max

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-N = int(input("entrer N :"))
-somme = 0
-for i in range(0 , N-1)
-print("entrer N° ")i+1
-somme = somme + liste{}
+tableau = []
+max_val = None
+min_val = None
+print("remplissage du tableau")
+for i in range(1 ,11) :
+    N = int(input(f"input(entrer N n°{i} : "))
+    tableau.append(N)
+    #print(f"taille tableau: {len(tableau)}")
+    if max_val is None or N > max_val :
+        max_val = N 
+    if min_val is None or N < min_val :
+       min_val = N
+print(f"le plus petit : {min_val}")
+print(f"le plus grand : {max_val}")
+print(f"tableau complet : {tableau}")
